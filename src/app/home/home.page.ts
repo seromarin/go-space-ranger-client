e41502c11170bd42afe4daead5ce41806732e1bc
Sebastián Rodríguez Marín <seromarin@MacBook-Air-de-Sebastian.local>
2019-04-30 10:45:51 -0500

    Call missions from backend and list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MissionsService } from '../services/missions.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  missions: Observable<any>;
+
+  constructor(
+    private missionService: MissionsService,
+  ) { }
+
+  ngOnInit() {
+    this.missions = this.missionService.getMissions();
+  }
 }
